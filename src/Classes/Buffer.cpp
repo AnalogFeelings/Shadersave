84284779570f20006fb1e0f35b124e5c8cb4fadd
AnalogFeelings <51166756+AnalogFeelings@users.noreply.github.com>
2024-10-05 00:31:31 +0200

    Forgot to initialize BufferTextureCopy.

--- src/Classes/Buffer.cpp
+++ src/Classes/Buffer.cpp
@@ -28,6 +28,11 @@ auto Buffer::SetupBuffer(unsigned int* textureGlobal, int viewportWidth, int vie
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+	glBindTexture(GL_TEXTURE_2D, this->BufferTextureCopy);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, viewportWidth, viewportHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
 	glBindFramebuffer(GL_FRAMEBUFFER, this->BufferFramebuffer);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, this->BufferTexture, 0);
 
